004b4d29263623c4fd827c4f979c36e0f0812be7
urielmayoravn <urielmayo@ravn.co>
2025-12-08 16:53:25 -0300

    change security_groups for vpc_security_group_ids

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -4,11 +4,11 @@ locals {
 }
 
 resource "aws_instance" "main" {
-  ami             = var.ami_id
-  instance_type   = var.instance_type
-  subnet_id       = var.subnet_id
-  security_groups = var.security_group_ids
-  key_name        = var.key_pair_name
-  tags            = { "Name" = var.Name }
-  user_data       = var.user_data_sh
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+  key_name               = var.key_pair_name
+  tags                   = { "Name" = var.Name }
+  user_data              = var.user_data_sh
 }
